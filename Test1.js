ee22238a6b95785fcd2968cc700dafb89c91e3dc
Rajitha Tupakula <rtupaku>
2019-12-06 23:05:20 -0800

    Or and And

--- Test1.js
+++ Test1.js
@@ -39,3 +39,83 @@ console.log(undefinedToBoolean);
 
 const nullToBoolean = !!null;
 console.log(`!!null is ${nullToBoolean}`);
+
+//Or and And
+console.log('***********')
+console.log("Check OR value")
+console.log('***********')
+let someValue = false || true;
+console.log(someValue);
+
+let someValue1 = false || true || false;
+console.log(someValue1);
+
+let someValue2 = false || ""
+console.log(someValue2);
+
+let someValue3 = "hello" || 0 || false; //result is Nan???
+console.log("hello is ", +someValue3);
+
+let someValue4 = "  " || "chicken" || {};
+console.log("chicken is " +someValue4);
+
+let someValue5 = {};
+console.log("{} is " +someValue5); //result is [object Object] instead of {}???
+
+let someValue6 = undefined;
+console.log(someValue6);
+
+let someValue7 = -1 || undefined;
+console.log(someValue7);
+
+let someValue8 = null || undefined || {} ;
+console.log(someValue8);
+
+let someValue9 = null || [] || {} ;
+console.log(someValue9);
+
+console.log('***********')
+console.log("Check AND value")
+console.log('***********')
+let Value = false &&  true;
+console.log(Value);
+
+let Value1 = false && true &&  false;
+console.log(Value1);
+
+let Value2 = false &&  "" ;
+console.log(Value2);
+
+let Value3 = "hello" &&  0  &&  false; //Should return false?? Returns 0
+console.log("hello" + Value3);
+
+let Value4 = "  " &&  "chicken" &&  {}; //
+console.log("chicken value " +Value4);
+
+let Value5 = {};
+console.log(Value5);
+
+let Value6 = undefined;
+console.log(Value6);
+
+let Value7 = -1 &&  undefined;
+console.log(Value7);
+
+let Value8 = null &&  undefined &&  {} ;
+console.log(Value8);
+
+let Value9 = null &&  [] &&  {} ;
+console.log("Value9 "+Value9);
+
+function hello () {
+    return false;
+}
+
+let Value10 = "yes" && function () { return false };
+console.log("value10 " +Value10);
+
+let Value11 = 1000 && hello();
+console.log(Value11);
+
+let Value12 = "yes" && (function  () { return undefined }()); //Need to understand
+console.log(Value12);
